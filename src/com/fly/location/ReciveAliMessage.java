582dc268ea97138ad3c762b9576cb9c322d1cbee
Vin <felix_21@163.com>
2018-11-17 00:24:10 +0800

    Update ReciveAliMessage.java

--- src/com/fly/location/ReciveAliMessage.java
+++ src/com/fly/location/ReciveAliMessage.java
@@ -11,9 +11,9 @@ import java.util.Properties;
 public class ReciveAliMessage {
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.put(PropertyKeyConst.ConsumerId, "CID_WEIWT_REC");// 您在MQ控制台创建的Consumer ID
-        properties.put(PropertyKeyConst.AccessKey, "LTAImMDTHhNAV6Ge");// 鉴权用AccessKey，在阿里云服务器管理控制台创建
-        properties.put(PropertyKeyConst.SecretKey, "PligF8lACHYUUQDrIxGCAQiQUI93MK");// 鉴权用SecretKey，在阿里云服务器管理控制台创建
+        properties.put(PropertyKeyConst.ConsumerId, "");// 您在MQ控制台创建的Consumer ID
+        properties.put(PropertyKeyConst.AccessKey, "");// 鉴权用AccessKey，在阿里云服务器管理控制台创建
+        properties.put(PropertyKeyConst.SecretKey, "");// 鉴权用SecretKey，在阿里云服务器管理控制台创建
         Consumer consumer = ONSFactory.createConsumer(properties);
         consumer.subscribe("location", "*", new MessageListener() {
             public Action consume(Message message, ConsumeContext context) {
